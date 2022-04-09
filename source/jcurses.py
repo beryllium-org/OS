f7167049dcc03e20574046a5bdc4144ca42bb03c
Bill Sideris <bill88t@feline.gr>
2022-04-09 22:21:21 +0300

    moved jcurses char_map to external file

--- source/jcurses.py
+++ source/jcurses.py
@@ -1,8 +1,8 @@
 from sys import stdout, stdin
 from supervisor import runtime
+from jcurses_data import char_map
 
 ESCK = "\033["
-
 
 class jcurses:
     def __init__(self):
@@ -30,114 +30,6 @@ class jcurses:
                     Valid values: "all" / "lettersnumbers" / "numbers" / "letters" / "common".
                 "echo": Can be "all" / "common" / "none".
             """
-
-    char_map = {  # you need to add 0x
-        "61": "a",  # smol letters
-        "62": "b",
-        "63": "c",
-        "64": "d",
-        "65": "e",
-        "66": "f",
-        "67": "g",
-        "68": "h",
-        "69": "i",
-        "6a": "j",
-        "6b": "k",
-        "6c": "l",
-        "6d": "m",
-        "6e": "n",
-        "6f": "o",
-        "70": "p",
-        "71": "q",
-        "72": "r",
-        "73": "s",
-        "74": "t",
-        "75": "u",
-        "76": "v",
-        "77": "w",
-        "78": "x",
-        "79": "y",
-        "7a": "z",
-        "41": "A",  # capital letters
-        "42": "B",
-        "43": "C",
-        "44": "D",
-        "45": "E",
-        "46": "F",
-        "47": "G",
-        "48": "H",
-        "49": "I",
-        "4a": "J",
-        "4b": "K",
-        "4c": "L",
-        "4d": "M",
-        "4e": "N",
-        "4f": "O",
-        "50": "P",
-        "51": "Q",
-        "52": "R",
-        "53": "S",
-        "54": "T",
-        "55": "U",
-        "56": "V",
-        "57": "W",
-        "58": "X",
-        "59": "Y",
-        "5a": "Z",
-        "31": "1",  # numbers
-        "32": "2",
-        "33": "3",
-        "34": "4",
-        "35": "5",
-        "36": "6",
-        "37": "7",
-        "38": "8",
-        "39": "9",
-        "30": "0",
-        "60": "`",  # from here on out it's random chars I captured
-        "2d": "-",
-        "5f": "_",
-        "3d": "=",
-        "2b": "+",
-        "5b": "[",
-        "5d": "]",
-        "5c": "\\",  # Never gonna give you up
-        "2f": "/",  # Never gonna let you down
-        "2e": ".",  # Never gonna run around and desert you
-        "2c": ",",  # Never gonna make you cry
-        "27": "'",  # Never gonna say goodbye
-        "3b": ";",  # Never gonna tell a lie and hurt you
-        "7b": "{",
-        "7d": "}",
-        "22": '"',
-        "3a": ":",
-        "7c": "|",
-        "3f": "?",
-        "3e": ">",
-        "3c": "<",
-        "1b": "alt",
-        "20": " ",
-        "a": "enter",
-        "9": "tab",
-        "7e": "~",
-        "4": "ctrlD",
-        "21": "!",
-        "40": "@",
-        "23": "#",
-        "24": "$",
-        "25": "%",
-        "5e": "^",
-        "26": "&",
-        "2a": "*",
-        "28": "(",
-        "29": ")",
-        "41": "up",
-        "42": "down",
-        "43": "right",
-        "44": "left",
-        "7f": "bck",
-        "ctrlC": "ctrlC",  # needed
-    }
 
     def backspace(self, n=1):
         for i in range(n):
@@ -260,7 +152,7 @@ class jcurses:
                         # Check for alt or process
                         if self.text_stepping is 0:
                             if charr != "1b":
-                                stack.append(self.char_map[charr])
+                                stack.append(char_map[charr])
                             else:
                                 self.text_stepping = 1
 
@@ -268,14 +160,14 @@ class jcurses:
                         elif self.text_stepping is 1:
                             if charr != "5b":
                                 self.text_stepping = 0
-                                stack.extend(["alt", self.char_map[charr]])
+                                stack.extend(["alt", char_map[charr]])
                             else:  # it's an arrow key
                                 self.text_stepping = 2
 
                         # Arrow keys
                         else:
                             self.text_stepping = 0
-                            stack.append(self.char_map[charr])
+                            stack.append(char_map[charr])
                     except KeyError:
                         pass
                     except KeyboardInterrupt:
