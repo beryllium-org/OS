e3338f730448a662749697430267d581f353cfae
Bill Sideris <bill88t@feline.gr>
2022-05-07 22:31:16 +0300

    added ccon installer without macos

---
+++ scripts/ccon.py
@@ -0,0 +1,13 @@
+from os import system, listdir, uname
+
+if uname().sysname == "Darwin":
+    pass
+elif uname().sysname == "Linux":
+    commd = "sudo cp ../scripts/screenningg.sh /bin/ccon"
+else:
+    print("Error: Unkown platform")
+    exit(1)
+
+print(f"Running: \"{commd}\"")
+system(commd)
+del commd
