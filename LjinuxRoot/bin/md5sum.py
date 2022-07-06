02ba6676a731f99c3adf14b7176af229cefccf7d
Bill Sideris <bill88t@feline.gr>
2022-07-06 16:07:25 +0300

    added all the sums

---
+++ LjinuxRoot/bin/md5sum.py
@@ -0,0 +1,25 @@
+try:
+    dataa = None
+    with open(
+        ljinux.based.fn.betterpath(ljinux.based.user_vars["argj"].split()[1]), "rb"
+    ) as f:
+        dataa = f.read()
+    a = """
+from adafruit_hashlib import md5
+a = md5()
+a.update(dataa)
+dataa = a.hexdigest()
+del a
+"""
+    exec(a, locals())
+    ljinux.based.user_vars["return"] = str(dataa)
+    del dataa
+    print(ljinux.based.user_vars["return"])
+
+except OSError:
+    ljinux.based.error(4, ljinux.based.user_vars["argj"].split()[1])
+    ljinux.based.user_vars["return"] = "1"
+
+except IndexError:
+    ljinux.based.error(1)
+    ljinux.based.user_vars["return"] = "1"
