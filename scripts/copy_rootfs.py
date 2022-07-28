c2f039cb67bdeec0f51ba296ade1b23fd338b761
RetiredWizard <github@retiredwizard.com>
2022-07-28 16:36:24 -0400

    Windows Install updates

---
+++ scripts/copy_rootfs.py
@@ -0,0 +1,50 @@
+from os import system, mkdir, listdir, path, popen
+from platform import uname
+from getpass import getuser
+
+if uname().system == "Linux":
+    slash = "/"
+    copy = "cp -rv"
+    copy2 = "cp -v" 
+else:
+    slash = "\\"
+    copy = "xcopy /y/s"
+    copy2 = "xcopy /y/s"
+ami = getuser()
+picop = ""
+if system(f"test -d /media/{ami}/LJINUX") == 0:
+    picop = f"/media/{ami}/LJINUX"
+elif system(f"test -d /media/{ami}/CIRCUITPY") == 0:
+    picop = f"/media/{ami}/CIRCUITPY"
+elif system("test -d /Volumes/LJINUX") == 0:
+    picop = "/Volumes/LJINUX"
+elif system("test -d /Volumes/CIRCUITPY") == 0:
+    picop = "/Volumes/CIRCUITPY"
+elif uname().system == 'Windows':
+    mpyn = f"..\scripts\mpy-cross-windows"
+    drives = [ chr(x) + ":" for x in range(65,91) if path.exists(chr(x) + ":") ]
+    for _ in drives:
+        vol = popen("vol "+_)
+        if vol.readline()[:-1].split(" ")[-1].upper() == "CIRCUITPY":
+            picop = f"%s" % _
+        vol.close()
+        if picop != "":
+            break
+if picop == "":
+    print(
+        "Error: Board not found.\nMake sure it is attached and mounted before you run make"
+    )
+    exit(1)
+
+print("\n[Copying root files\n")
+if uname().system == "Linux":
+    print("cp -rv ../LjinuxRoot/* "+picop+"/")
+    system(f"cp -rv ../LjinuxRoot/* {picop}/")
+    print("cp -v ../Manual.txt "+picop+"/LjinuxRoot/home/pi/")
+    system(f"cp -v ../Manual.txt {picop}/LjinuxRoot/home/pi/")
+else:
+    print("xcopy /y/s ..\\LjinuxRoot\\* "+picop+"\\LjinuxRoot\\")
+    system(f"xcopy /y/s ..\\LjinuxRoot\\* {picop}\\LjinuxRoot\\")
+    print("copy ..\\Manual.txt "+picop+"\\LjinuxRoot\\home\\pi\\")
+    system(f"copy ..\\Manual.txt {picop}\\LjinuxRoot\\home\\pi\\")
+
--- scripts/make_kernel.py
+++ scripts/make_kernel.py
@@ -1,9 +1,16 @@
-from os import system, mkdir, listdir, uname
+from os import system, mkdir, listdir, path, popen
+from platform import uname
 from getpass import getuser
 
+if uname().system == "Linux":
+    slash = "/"
+    copy = "cp" 
+else:
+    slash = "\\"
+    copy = "copy"
 ami = getuser()
 mpyn = f"../scripts/mpy-cross-{uname().machine}"
-print(f"\nUsing mpycross: {mpyn}")
+picop = ""
 if system(f"test -d /media/{ami}/LJINUX") == 0:
     picop = f"/media/{ami}/LJINUX"
 elif system(f"test -d /media/{ami}/CIRCUITPY") == 0:
@@ -12,23 +19,35 @@ elif system("test -d /Volumes/LJINUX") == 0:
     picop = "/Volumes/LJINUX"
 elif system("test -d /Volumes/CIRCUITPY") == 0:
     picop = "/Volumes/CIRCUITPY"
-else:
+elif uname().system == 'Windows':
+    mpyn = f"..\scripts\mpy-cross-windows"
+    drives = [ chr(x) + ":" for x in range(65,91) if path.exists(chr(x) + ":") ]
+    for _ in drives:
+        vol = popen("vol "+_)
+        if vol.readline()[:-1].split(" ")[-1].upper() == "CIRCUITPY":
+            picop = f"%s" % _
+        vol.close()
+        if picop != "":
+            break
+if picop == "":
     print(
         "Error: Board not found.\nMake sure it is attached and mounted before you run make"
     )
     exit(1)
 
+print(f"\nUsing mpycross: {mpyn}")
+
 print(f"Using board path: {picop}\n")
-if system(f"test -d {picop}/lib") != 0:
+if system(f"test -d {picop}/lib".replace("/",slash)) != 0:
     print("Created lib directory.")
-    mkdir(f"{picop}/lib")
+    mkdir(f"{picop}/lib".replace("/",slash))
 
 print("[1/4] Compiling source files and pin allocation tabs\n")
 for filee in listdir():
     if filee.endswith(".py"):
         print(f"-> {filee[:-3]}")
         a = system(
-            f"{mpyn} ./{filee} -s {filee[:-3]} -v -O4 -o {picop}/lib/{filee[:-3]}.mpy"
+            f"{mpyn} ./{filee} -s {filee[:-3]} -v -O4 -o {picop}/lib/{filee[:-3]}.mpy".replace("/",slash)
         )
         if a != 0:
             print("Compilation error, exiting")
@@ -36,25 +55,25 @@ for filee in listdir():
         del a
 
 print("\n[2/4] Copying base files\n")
-for filee in listdir("../rootfilesystem/"):
+for filee in listdir("../rootfilesystem/".replace("/",slash)):
     print(f"-> {filee}")
-    system(f"cp ../rootfilesystem/{filee} {picop}/")
+    system(f"cp ../rootfilesystem/{filee} {picop}/".replace("/",slash).replace("cp",copy))
 
 print("\n[3/4] Copying Adafruit hashlib files\n")
-if system(f"test -d {picop}/lib/adafruit_hashlib") != 0:
+if system(f"test -d {picop}/lib/adafruit_hashlib".replace("/",slash)) != 0:
     print("Created adafruit_hashlib directory.")
-    mkdir(f"{picop}/lib/adafruit_hashlib")
-for filee in listdir("../other/adafruit_hashlib"):
+    mkdir(f"{picop}/lib/adafruit_hashlib".replace("/",slash))
+for filee in listdir("../other/adafruit_hashlib".replace("/",slash)):
     print(f"-> {filee}")
-    system(f"cp ../other/adafruit_hashlib/{filee} {picop}/lib/adafruit_hashlib/")
+    system(f"cp ../other/adafruit_hashlib/{filee} {picop}/lib/adafruit_hashlib/".replace("/",slash).replace("cp",copy))
 
 print("\n[4/4] Copying Adafruit hid files\n")
-if system(f"test -d {picop}/lib/adafruit_hid") != 0:
+if system(f"test -d {picop}/lib/adafruit_hid".replace("/",slash)) != 0:
     print("Created adafruit_hid directory.")
-    mkdir(f"{picop}/lib/adafruit_hid")
-for filee in listdir("../other/adafruit_hid"):
+    mkdir(f"{picop}/lib/adafruit_hid".replace("/",slash))
+for filee in listdir("../other/adafruit_hid".replace("/",slash)):
     print(f"-> {filee}")
-    system(f"cp ../other/adafruit_hid/{filee} {picop}/lib/adafruit_hid/")
+    system(f"cp ../other/adafruit_hid/{filee} {picop}/lib/adafruit_hid/".replace("/",slash).replace("cp",copy))
 
 system("sync")
 print()
