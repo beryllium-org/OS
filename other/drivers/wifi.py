8def30082a01cc9955d8ae4d666ea661f77589eb
bill88t <bill88t@feline.gr>
2022-08-11 23:08:57 +0300

    more wifi progress

--- other/drivers/wifi.py
+++ other/drivers/wifi.py
@@ -1,43 +1,54 @@
-import time
-import ipaddress
+from time import sleep
+from ipaddress import ip_address
 import wifi
-import socketpool
-import ssl
-import adafruit_requests
+from socketpool import SocketPool
+from ssl import create_default_context
+from adafruit_requests import Session
 
 
-class ljinux_wifi:
+class driver_wifi:
     """
     Ljinux network driver for the built in wifi module
     """
 
     def __init__(self):
-        self.interface = None
-        self.started = False
+        # internal use only
+        self._pool = None
+        self._session = None
+        
+        # public
         self.error = False
         self.connected = False
         self.up = False
+        self.internet = False
 
-    def start(self):
-        pass
-
-    def deinit(self):
-        pass
-
-    def int_connect(self, data=None):
-        pass
+    def connect(self, ssid, passwd):
+        try:
+            wifi.radio.connect(ssid=ssid,password=passwd)
+        except ConnectionError:
+            return 1
+        self._pool = SocketPool(wifi.radio)
+        self._session = Session(self._pool, create_default_context())
+        return 0
 
     def ping(self, host):
-        pass
+        return wifi.radio.ping(self.resolve(host))
 
     def get(self, host):
-        pass
+        return self._session.get(host)
 
     def resolve(self, host):
-        pass
-
-    def reset(self):
-        pass
+        return ip_address(host)
 
     def scan(self):
+        netnames = []
+        for network in wifi.radio.start_scanning_networks():
+            netnames.append(network.ssid)
+        wifi.radio.stop_scanning_networks()
+        return netnames
+
+    def get_ipconf(self):
         pass
+    
+    def disconnect(self):
+        wifi.radio.stop_station()
