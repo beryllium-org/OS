576847f2c74ef78290337ea6730da6281819343f
bill88t <bill88t@feline.gr>
2022-08-20 19:39:47 +0300

    jz accept silent

--- LjinuxRoot/bin/jz.py
+++ LjinuxRoot/bin/jz.py
@@ -1,5 +1,11 @@
 args = ljinux.based.user_vars["argj"].split()[1:]
 argl = len(args)
+quiett = False
+if argl > 1 and args[0] == "quiet":
+    quiett = True
+    args = args[1:]
+    argl -= 1
+
 if argl > 1 and args[0] == "decompress":
     from jz import decompress
 
@@ -7,7 +13,7 @@ if argl > 1 and args[0] == "decompress":
     unzpath = "." if argl is 2 else args[2]
     if not sdcard_fs:
         remount("/", False)
-    decompress(zname, unzpath)
+    decompress(zname, unzpath, quiet=quiett)
     if not sdcard_fs:
         remount("/", True)
     del decompress, zname, unzpath
@@ -17,4 +23,4 @@ else:
     ljinux.based.error(1)
     ljinux.based.user_vars["return"] = "1"
 
-del args, argl
+del args, argl, quiett
