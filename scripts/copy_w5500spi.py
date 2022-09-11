0738280646d1b67212c8448afa833d203b803a53
bill88t <bill88t@feline.gr>
2022-09-11 20:37:33 +0300

    w5500 module and stuff

--- scripts/copy_w5500spi.py
+++ scripts/copy_w5500spi.py
@@ -38,7 +38,7 @@ if system(f"test -d {picop}/lib/drivers".replace("/", slash)) != 0:
     print("Created lib/drivers directory.")
     mkdir(f"{picop}/lib/drivers".replace("/", slash))
 
-print("\n[1/4] Compiling Adafruit ntp\n-> adafruit_ntp")
+print("\n[1/5] Compiling Adafruit ntp\n-> adafruit_ntp")
 a = system(
     f"{mpyn} ../other/Adafruit_CircuitPython_NTP/adafruit_ntp.py -s adafruit_ntp -v -O4 -o {picop}/lib/adafruit_ntp.mpy".replace(
         "/", slash
@@ -46,9 +46,8 @@ a = system(
 )
 if a != 0:
     errexit()
-del a
 
-print("\n[2/4] Compiling adafruit requests\n-> adafruit_requests")
+print("\n[2/5] Compiling Adafruit requests\n-> adafruit_requests")
 a = system(
     f"{mpyn} ../other/Adafruit_CircuitPython_Requests/adafruit_requests.py -s adafruit_requests -v -O4 -o {picop}/lib/adafruit_requests.mpy".replace(
         "/", slash
@@ -56,9 +55,8 @@ a = system(
 )
 if a != 0:
     errexit()
-del a
 
-print("\n[3/4] Compiling adafruit HTTPServer\n-> adafruit_httpserver")
+print("\n[3/5] Compiling Adafruit HTTPServer\n-> adafruit_httpserver")
 a = system(
     f"{mpyn} ../other/Adafruit_CircuitPython_HTTPServer/adafruit_httpserver.py -s adafruit_httpserver -v -O4 -o {picop}/lib/adafruit_httpserver.mpy".replace(
         "/", slash
@@ -66,9 +64,25 @@ a = system(
 )
 if a != 0:
     errexit()
-del a
 
-print("\n[4/4] Compiling w5500 spi drivers\n-> driver_w5500spi")
+print("\n[4/5] Compiling Adafruit CircuitPython Wiznet5k\n")
+if system(f"test -d {picop}/lib/adafruit_wiznet5k".replace("/", slash)) != 0:
+    print("Created adafruit_wiznet5k directory.")
+    mkdir(f"{picop}/lib/adafruit_wiznet5k".replace("/", slash))
+for filee in listdir(
+    "../other/Adafruit_CircuitPython_Wiznet5k/adafruit_wiznet5k/".replace("/", slash)
+):
+    if filee != "adafruit_wiznet5k_wsgiserver.py":
+        print(f"-> {filee[:-3]}")
+        a = system(
+            f"{mpyn} ../other/Adafruit_CircuitPython_Wiznet5k/adafruit_wiznet5k/{filee} -s {filee[:-3]} -v -O4 -o {picop}/lib/adafruit_wiznet5k/{filee[:-3]}.mpy".replace(
+                "/", slash
+            )
+        )
+        if a != 0:
+            errexit()
+
+print("\n[5/5] Compiling w5500 spi drivers\n-> driver_w5500spi")
 a = system(
     f"{mpyn} ../other/drivers/w5500spi.py -s driver_w5500spi -v -O4 -o {picop}/lib/drivers/driver_w5500spi.mpy".replace(
         "/", slash
@@ -76,8 +90,6 @@ a = system(
 )
 if a != 0:
     errexit()
-del a
 
 system("sync")
 print()
-del ami, picop, mpyn
