a6500000675cf21a2d17ca0b5309c5032948c1ba
Bill Sideris <bill88t@feline.gr>
2022-09-20 18:58:13 +0300

    add windows scripts for drivers

---
+++ source/windows-w5500.bat
@@ -0,0 +1,3 @@
+@echo "Installing w5500 spi driver"
+@python3 ../scripts/copy_w5500spi.py
+@echo "w5500 spi driver done"
---
+++ source/windows-wifi.bat
@@ -0,0 +1,3 @@
+@echo "Installing wifi driver"
+@python3 ../scripts/copy_wifi.py
+@echo "Wifi driver done"
