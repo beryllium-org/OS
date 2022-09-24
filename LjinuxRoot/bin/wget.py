ff54ea9f188002a73e155efc862e586d8e6a88cb
Bill Sideris <bill88t@feline.gr>
2022-09-24 19:05:44 +0300

    wget updates

--- LjinuxRoot/bin/wget.py
+++ LjinuxRoot/bin/wget.py
@@ -1,19 +1,17 @@
 args = ljinux.based.user_vars["argj"].split()
+argc = len(args)
 if "network" in ljinux.modules and ljinux.modules["network"].connected:
-    try:
-        global sdcard_fs
-        if not sdcard_fs:
-            remount("/", False)
-        try:
-            with ljinux.based.fn.fopen(args[2], "wb") as filee:
+    if argc > 1:
+        nam = args[1][args[1].rfind("/") + 1 :] if argc < 3 else args[2]
+        with ljinux.based.fn.fopen(nam, "wb") as filee:
+            if filee is not None:
                 filee.write(ljinux.modules["network"].get(args[1]).content)
-            ljinux.modules["network"].resetsock()
-        except IndexError:
-            ljinux.based.error(9)
-        if not sdcard_fs:
-            remount("/", True)
-    except RuntimeError:
-        ljinux.based.error(7)
+            else:
+                ljinux.based.error(7)
+        ljinux.modules["network"].resetsock()
+        del nam
+    else:
+        ljinux.based.error(9)
 else:
     ljinux.based.error(5)
-del args
+del args, argc
