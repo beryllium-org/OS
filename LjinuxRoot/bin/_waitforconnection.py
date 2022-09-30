93c1c5b7f3325c211f5ec45c67e713c8ea0f6861
Bill Sideris <bill88t@feline.gr>
2022-09-30 21:52:56 +0300

    _waitforconnection replaces the old stuff, fully automatic

---
+++ LjinuxRoot/bin/_waitforconnection.py
@@ -0,0 +1,9 @@
+if not hasattr(console, "fake"):
+    while not console.connected:
+        ljinux.io.ledset(4)
+        time.sleep(0.4)
+        ljinux.io.ledset(7)
+        time.sleep(0.4)
+    time.sleep(0.6)
+else:
+    term.anykey("\nCannot autodetect connection\nPress any key to open based shell\n")
