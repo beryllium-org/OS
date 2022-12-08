f04d2b84c9fd91230edb7a8f3831e8f1f6992ede
Bill Sideris <bill88t@feline.gr>
2022-12-08 17:09:22 +0200

    .

--- scripts/copy_rootfs.py
+++ scripts/copy_rootfs.py
@@ -1,34 +1,35 @@
 from os import system, mkdir, listdir, path, popen
 from platform import uname
 from getpass import getuser
-from detect_board import detect_board
+from sys import path as spath
 
-[picop, board, version] = detect_board()
+spath.append("../scripts/CircuitMPY/")
+import circuitmpy
 
-if picop == "":
+[boardpath, board, version] = circuitmpy.detect_board()
+
+if boardpath == "":
     print(
         "Error: Board not found.\nMake sure it is attached and mounted before you run make"
     )
     exit(1)
 
 if uname().system == "Linux":
-    if system(f"test -d {picop}/LjinuxRoot") != 0:
+    if system(f"test -d {boardpath}/LjinuxRoot") != 0:
         print("Created LjinuxRoot")
-        mkdir(f"{picop}/LjinuxRoot")
+        mkdir(f"{boardpath}/LjinuxRoot")
     print("[1/3] Updating /LjinuxRoot")
-    system(f"rsync -r --update ../LjinuxRoot/* {picop}/LjinuxRoot/")
-    # print("[2/4] Installing Manual.")
-    # system(f"rsync --update ../Manual.txt {picop}/LjinuxRoot/home/board/")
+    system(f"rsync -r --update ../LjinuxRoot/* {boardpath}/LjinuxRoot/")
     print("[2/3] Installing board pinout map.")
     system(
-        f"rsync --update ../Boardfiles/{board}/pinout.map {picop}/LjinuxRoot/bin/pinout.map"
+        f"rsync --update ../Boardfiles/{board}/pinout.map {boardpath}/LjinuxRoot/bin/pinout.map"
     )
     print("[3/3] Updating boot configuration")
-    if system(f"test -d {picop}/LjinuxRoot/boot") != 0:
-        mkdir(f"{picop}/LjinuxRoot/boot")
-    system(f"rsync -r --update ../bootcfg/* {picop}/LjinuxRoot/boot/")
+    if system(f"test -d {boardpath}/LjinuxRoot/boot") != 0:
+        mkdir(f"{boardpath}/LjinuxRoot/boot")
+    system(f"rsync -r --update ../bootcfg/* {boardpath}/LjinuxRoot/boot/")
 else:
-    system(f"xcopy /y/s ..\\LjinuxRoot\\* {picop}\\LjinuxRoot\\")
-    system(f"xcopy /y/s/d ..\\LjinuxRoot\\boot\\* {picop}\\LjinuxRoot\\boot\\")
-    system(f"copy ..\\Manual.txt {picop}\\LjinuxRoot\\home\\board\\")
+    system(f"xcopy /y/s ..\\LjinuxRoot\\* {boardpath}\\LjinuxRoot\\")
+    system(f"xcopy /y/s/d ..\\LjinuxRoot\\boot\\* {boardpath}\\LjinuxRoot\\boot\\")
+    system(f"copy ..\\Manual.txt {boardpath}\\LjinuxRoot\\home\\board\\")
 system("sync")
