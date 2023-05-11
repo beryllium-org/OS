d3cf822325948652507dcb9c8595d374dde314f6
Bill Sideris <bill88t@feline.gr>
2023-05-11 18:47:30 +0300

    redo rmdir

--- LjinuxRoot/bin/rmdir.py
+++ LjinuxRoot/bin/rmdir.py
@@ -1,23 +1,30 @@
 global sdcard_fs
-try:
-    if not sdcard_fs:
-        remount("/", False)
-    rmdir(ljinux.api.betterpath(ljinux.based.user_vars["argj"].split()[1]))
-    if not sdcard_fs:
-        remount("/", True)
-except (OSError, RuntimeError) as errr:
-    if str(errr) == "[Errno 2] No such file/directory":
-        term.write(
-            "rmdir: failed to remove ‘"
-            + ljinux.based.user_vars["argj"].split()[1]
-            + "’: No such file or directory"
-        )
-    else:
-        term.write(
-            "rmdir: failed to remove ‘"
-            + ljinux.based.user_vars["argj"].split()[1]
-            + "’: Cannot write, the pi pico is in read only mode!\nMake sure to disable to usb drive to be able to access these functions!"
-        )
-    del errr
-except IndexError:
-    pass
+opts = ljinux.api.xarg()
+if ("help" in opts["o"]) or ("h" in opts["o"]):
+    ljinux.api.setvar("argj", "a /usr/share/help/rmdir.txt")
+    ljinux.based.command.fpexec("/bin/cat.py")
+    ljinux.api.setvar("return", "1")
+else:
+    if len(opts["w"]) > 0:
+        for i in opts["w"]:
+            if ljinux.api.isdir(i) == 1:
+                pr = ljinux.api.betterpath(i)
+                if not len(listdir(pr)):
+                    try:
+                        if not sdcard_fs:
+                            remount("/", False)
+                        try:
+                            rmdir(ljinux.api.betterpath(pr))
+                        except OSError:
+                            ljinux.based.error(4, "not done", prefix="rmdir")
+                        if not sdcard_fs:
+                            remount("/", True)
+                    except RuntimeError:
+                        ljinux.based.error(7, prefix="rmdir")
+                else:
+                    ljinux.based.error(16, prefix="rmdir")
+                del pr
+            else:
+                ljinux.based.error(17, prefix="rmdir")
+            del i
+del opts
