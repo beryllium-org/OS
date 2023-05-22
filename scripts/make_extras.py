b33bcbeb8dde49d71c4a0702cad9cc33109e4d6d
Bill Sideris <bill88t@feline.gr>
2023-05-22 22:37:49 +0300

    Remove windows scripts, new extras system Removes wifi script, uses extras now drivers are seperate virtuart handled with extras

---
+++ scripts/make_extras.py
@@ -0,0 +1,93 @@
+from os import system, mkdir, listdir, path, getcwd
+from sys import argv
+from sys import path as spath
+
+spath.append("../scripts/CircuitMPY/")
+spath.append("../other/cptoml/")
+import circuitmpy
+from cptoml import fetch
+
+
+def errexit():
+    print("Compilation error, exiting")
+    exit(1)
+
+
+optimis = 3
+try:
+    if argv[1] == "debug":
+        print("Alert: Compiling with debug enabled.")
+        optimis = 0
+except IndexError:
+    pass
+
+[boardpath, board, version] = circuitmpy.detect_board()
+
+if boardpath == None:
+    print(
+        "Error: Board not found.\nMake sure it is attached and mounted before you run make"
+    )
+    exit(1)
+
+if system(f"test -d {boardpath}/lib") != 0:
+    mkdir(f"{boardpath}/lib")
+if system(f"test -d {boardpath}/lib/drivers") != 0:
+    mkdir(f"{boardpath}/lib/drivers")
+
+if path.exists(f"../Boardfiles/{board}/extras"):
+    for i in listdir(f"../Boardfiles/{board}/extras/"):
+        if i.endswith(".other"):
+            source_p = "../other/" + i[:-6].replace(".", "/")
+            target_p = f"{boardpath}/lib/" + source_p[source_p.rfind("/") + 1 :]
+            print(f"[-/-] Compiling extras: {source_p}")
+            if path.isdir(source_p):
+                if not path.exists(target_p):
+                    mkdir(target_p)
+                for filee in listdir(source_p):
+                    circuitmpy.compile_mpy(
+                        f"{source_p}/{filee}",
+                        f"{target_p}/{filee[:-3]}.mpy",
+                        optim=optimis,
+                    )
+            elif path.isfile(source_p + ".py"):
+                try:
+                    circuitmpy.compile_mpy(
+                        f"../other/{source_p}.py", f"{target_p}.mpy", optim=optimis
+                    )
+                except OSError:
+                    errexit()
+            else:
+                errexit()
+            del source_p, target_p
+        elif i.endswith(".py"):
+            print(f"[-/-] Compiling extras: extras/{i[:-2]}")
+            if path.isdir(f"../Boardfiles/{board}/extras/{i}"):
+                print("NOT IMPLEMENTED")
+                errexit()
+            elif path.isfile(f"../Boardfiles/{board}/extras/{i}"):
+                try:
+                    circuitmpy.compile_mpy(
+                        f"../Boardfiles/{board}/extras/{i}",
+                        f"{boardpath}/lib/{i[:-3]}.mpy",
+                        optim=optimis,
+                    )
+                except OSError:
+                    errexit()
+        elif i.endswith(".driver"):
+            print(f"[-/-] Compiling extras: drivers/{i[:-7]}")
+            if path.isdir(f"../Boardfiles/{board}/extras/{i}"):
+                print("NOT IMPLEMENTED")
+                errexit()
+            elif path.isfile(f"../Boardfiles/{board}/extras/{i}"):
+                try:
+                    circuitmpy.compile_mpy(
+                        f"../other/drivers/{i[:-7]}.py",
+                        f"{boardpath}/lib/drivers/{i[:-7]}.mpy",
+                        optim=optimis,
+                    )
+                except OSError:
+                    errexit()
+        else:
+            errexit()
+
+system("sync")
--- scripts/make_kernel.py
+++ scripts/make_kernel.py
@@ -1,6 +1,4 @@
-from os import system, mkdir, listdir, path, popen, getcwd
-from platform import uname
-from getpass import getuser
+from os import system, mkdir, listdir
 from sys import argv
 from sys import path as spath
 
@@ -23,13 +21,6 @@ try:
 except IndexError:
     pass
 
-if uname().system == "Linux":
-    slash = "/"
-    copy = "rsync -h"
-else:
-    slash = "\\"
-    copy = "copy"
-
 [boardpath, board, version] = circuitmpy.detect_board()
 
 if boardpath == None:
@@ -41,44 +32,40 @@ if boardpath == None:
 print(f"Using board path: {boardpath}")
 print(f"Building for board: {board}\n")
 
-if system(f"test -d {boardpath}/lib".replace("/", slash)) != 0:
-    mkdir(f"{boardpath}/lib".replace("/", slash))
-if system(f"test -d {boardpath}/lib/drivers".replace("/", slash)) != 0:
-    mkdir(f"{boardpath}/lib/drivers".replace("/", slash))
+if system(f"test -d {boardpath}/lib") != 0:
+    mkdir(f"{boardpath}/lib")
+if system(f"test -d {boardpath}/lib/drivers") != 0:
+    mkdir(f"{boardpath}/lib/drivers")
 
-print("[1/7] Compiling source files")
+print("[1/6] Compiling source files")
 for filee in listdir():
     if filee.endswith(".py"):
         try:
             circuitmpy.compile_mpy(
-                f"./{filee}".replace("/", slash),
-                f"{boardpath}/lib/{filee[:-3]}.mpy".replace("/", slash),
+                f"./{filee}",
+                f"{boardpath}/lib/{filee[:-3]}.mpy",
                 optim=optimis,
             )
         except OSError:
             errexit()
 
-print("[2/7] Compiling jcurses")
+print("[2/6] Compiling jcurses")
 for filee in listdir("jcurses"):
     if filee.endswith(".py"):
         try:
             circuitmpy.compile_mpy(
-                f"./jcurses/{filee}".replace("/", slash),
-                f"{boardpath}/lib/{filee[:-3]}.mpy".replace("/", slash),
+                f"./jcurses/{filee}",
+                f"{boardpath}/lib/{filee[:-3]}.mpy",
                 optim=optimis,
             )
         except OSError:
             errexit()
 
-print("[3/7] Copying base files")
-for filee in listdir("../rootfilesystem/".replace("/", slash)):
-    system(
-        f"cp ../rootfilesystem/{filee} {boardpath}/".replace("/", slash).replace(
-            "cp", copy
-        )
-    )
+print("[3/6] Copying base files")
+for filee in listdir("../rootfilesystem/"):
+    system(f"cp ../rootfilesystem/{filee} {boardpath}/")
 
-print("[4/7] Copying board configuration files")
+print("[4/6] Copying board configuration files")
 skiptm = False
 try:
     if fetch("setup", "LJINUX", toml=f"{boardpath}/settings.toml"):
@@ -86,11 +73,7 @@ try:
 except:
     pass
 if not skiptm:
-    system(
-        f"cp ../Boardfiles/{board}/settings.toml {boardpath}/settings.toml".replace(
-            "/", slash
-        ).replace("cp", copy)
-    )
+    system(f"cp ../Boardfiles/{board}/settings.toml {boardpath}/settings.toml")
 else:
     print(" - Skipped updating toml as setup variable already True")
 try:
@@ -100,29 +83,18 @@ try:
 except OSError:
     errexit()
 
-print("[5/7] Compiling jz")
+print("[5/6] Compiling jz")
 try:
     circuitmpy.compile_mpy("./jz/jz.py", f"{boardpath}/lib/jz.mpy", optim=optimis)
 except OSError:
     errexit()
 
-print("[6/7] Compiling cptoml")
+print("[6/6] Compiling cptoml")
 try:
     circuitmpy.compile_mpy(
         "../other/cptoml/cptoml.py", f"{boardpath}/lib/cptoml.mpy", optim=optimis
     )
 except OSError:
     errexit()
-
-
-print("[6/7] Checking for additional requirements")
-if path.exists(f"../Boardfiles/{board}/needs_virtUART".replace("/", slash)):
-    print("This board needs virtUART\n\n[1/1] Compiling virtUART")
-    try:
-        circuitmpy.compile_mpy(
-            "../other/virtUART.py", f"{boardpath}/lib/virtUART.mpy", optim=optimis
-        )
-    except OSError:
-        errexit()
 
 system("sync")
