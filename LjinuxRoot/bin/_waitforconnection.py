3dc479e5227a6bf2508a93951ae7fc9ab9c78aa0
Bill Sideris <bill88t@feline.gr>
2023-06-06 02:40:04 +0300

    Faster _waitforconnection

--- LjinuxRoot/bin/_waitforconnection.py
+++ LjinuxRoot/bin/_waitforconnection.py
@@ -1,9 +1,15 @@
-if term.check_activity():
+if term.check_activity():  # has .connected
     while not console.connected:
         ljinux.io.ledset(4)
-        time.sleep(0.4)
+        for i in range(4):
+            if console.connected:
+                break
+            time.sleep(0.1)
         ljinux.io.ledset(7)
-        time.sleep(0.4)
-    time.sleep(0.6)
+        for i in range(4):
+            if console.connected:
+                break
+            time.sleep(0.1)
+    time.sleep(0.2)  # Delay for the terminal to get used to it.
 else:
     term.anykey("\nCannot autodetect connection\nPress any key to open based shell\n")
