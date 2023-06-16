d164d1e06199f21b41c007b0181cfcc5eb33123c
Bill Sideris <bill88t@feline.gr>
2023-06-16 17:41:39 +0300

    fix `reboot bootloader` on non .connected consoles.

--- LjinuxRoot/bin/reboot.py
+++ LjinuxRoot/bin/reboot.py
@@ -5,14 +5,15 @@ Exit = True
 try:
     if inpt[1] == "bootloader":
         Exit_code = 243
-        term.write(
-            "Please disconnect from serial to continue..\nTo continue anyways, press Ctrl + C"
-        )
-        try:
-            while console.connected:
-                time.sleep(0.2)
-        except KeyboardInterrupt:
-            pass
+        if hasattr(term.console, "connected"):
+            term.write(
+                "Please disconnect from serial to continue..\nTo continue anyways, press Ctrl + C"
+            )
+            try:
+                while console.connected:
+                    time.sleep(0.2)
+            except KeyboardInterrupt:
+                pass
     elif inpt[1] == "safemode":
         Exit_code = 242
     elif inpt[1] == "uf2":
