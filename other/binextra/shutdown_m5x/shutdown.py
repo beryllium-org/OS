244239e0012331234db0fb18401a13a8730ffbb8
Bill Sideris <bill88t@feline.gr>
2023-06-17 03:08:37 +0300

    Less commands for m5x shutdown

--- other/binextra/shutdown_m5x/shutdown.py
+++ other/binextra/shutdown_m5x/shutdown.py
@@ -1,9 +1,7 @@
 ljinux.io.ledset(1)
 ljinux.deinit_consoles()
 sleep(1)
-from board import BAT_HOLD
-
-a = digitalio.DigitalInOut(BAT_HOLD)
+a = digitalio.DigitalInOut(board.BAT_HOLD)
 a.switch_to_output()
 a.value = 0
 while True:
