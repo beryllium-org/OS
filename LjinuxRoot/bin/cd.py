a4773ed44a67ef29229b795579d5d06c8035f738
Bill Sideris <bill88t@feline.gr>
2023-12-08 19:05:23 +0200

    Rewritten cd to use xarg

--- LjinuxRoot/bin/cd.py
+++ LjinuxRoot/bin/cd.py
@@ -1,20 +1,19 @@
 rename_process("cd")
 vr("capdir", getcwd())
-try:
-    vr("dirr", ljinux.api.getvar("argj").split()[1])
-    if vr("dirr") != "-":
-        vr("dr", ljinux.api.isdir(vr("dirr")))
-        if vr("dr") == 1:
-            chdir(ljinux.api.betterpath(vr("dirr")))
-            if vr("capdir") != getcwd():
-                ljinux.api.setvar("prevdir", vr("capdir"))
-        elif not vr("dr"):
-            ljinux.based.error(14, "cd")
-        else:
-            ljinux.based.error(17, "cd")
-    else:
-        chdir(ljinux.based.user_vars["prevdir"])
+vr("opts", ljinux.api.xarg())
+if len(vr("opts")["w"]):
+    vr("target", vr("opts")["w"][0])
+    if vr("target") == "-":
+        vr("target", ljinux.based.user_vars["prevdir"])
+else:
+    vr("target", "~")
+vr("dr", ljinux.api.isdir(vr("target")))
+if vr("dr") == 1:
+    chdir(ljinux.api.betterpath(vr("target")))
+    if vr("capdir") != getcwd():
         ljinux.api.setvar("prevdir", vr("capdir"))
-    ljinux.based.olddir = getcwd()
-except IndexError:
-    pass
+        ljinux.based.olddir = getcwd()
+elif not vr("dr"):
+    ljinux.based.error(14, "cd")
+else:
+    ljinux.based.error(17, "cd")
