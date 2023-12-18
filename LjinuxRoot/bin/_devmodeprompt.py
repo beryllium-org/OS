00a8f407a8500e4ebd6b36ed14812487a52477da
Bill Sideris <bill88t@feline.gr>
2023-12-18 23:20:38 +0200

    Update the dev mode prompt

--- LjinuxRoot/bin/_devmodeprompt.py
+++ LjinuxRoot/bin/_devmodeprompt.py
@@ -2,10 +2,10 @@ if cptoml.fetch("usb_msc_available", "LJINUX") and cptoml.fetch(
     "usb_msc_enabled", "LJINUX"
 ):
     term.write(
-        f"""{colors.red_t}\nWarning:{colors.endc} Developer mode has been enabled!
+        f"""{colors.red_t}\nWarning:{colors.endc} USB filesystem is enabled!
 While it is enabled, Ljinux will only be able to
 access the file system {colors.red_t}READ ONLY{colors.endc}.
 
-To disable it, {colors.red_t}set usb_access to 'false'{colors.endc} in '&/settings.toml'!
+To disable it, {colors.red_t}set usb_msc_enabled to 'false'{colors.endc} in '&/settings.toml'!
 """
     )
