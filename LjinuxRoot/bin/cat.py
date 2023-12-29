96069d8ceb1feb82a7e0048380b84653adf1b1fc
Bill Sideris <bill88t@feline.gr>
2023-12-30 01:26:34 +0200

    Update cat to better work with fopen

--- LjinuxRoot/bin/cat.py
+++ LjinuxRoot/bin/cat.py
@@ -3,15 +3,14 @@ vr("inpt", ljinux.based.user_vars["argj"].split())
 
 try:
     with ljinux.api.fopen(vr("inpt")[1], "r") as pv[get_pid()]["f"]:
-        for pv[get_pid()]["line"] in vr("f"):
-            term.nwrite(vr("line"))
+        if vr("f") is None:
+            ljinux.based.error(4, vr("inpt")[1])
+            ljinux.api.setvar("return", "1")
+        else:
+            for pv[get_pid()]["line"] in vr("f"):
+                term.nwrite(vr("line"))
             vrd("line")
-    ljinux.api.setvar("return", "0")
-
-except OSError:
-    ljinux.based.error(4, vr("inpt")[1])
-    ljinux.api.setvar("return", "1")
-
+            ljinux.api.setvar("return", "0")
 except IndexError:
     ljinux.based.error(1)
     ljinux.api.setvar("return", "1")
