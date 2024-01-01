8439634441b5b229ab68cd7763e27ff2fa17fcaa
Bill Sideris <bill88t@feline.gr>
2024-01-01 13:42:08 +0200

    Add the disconnect hook in disconnect command

--- LjinuxRoot/bin/disconnect.py
+++ LjinuxRoot/bin/disconnect.py
@@ -2,6 +2,7 @@ if hasattr(term.console, "disconnect"):
     term.console.disconnect()
 elif term._active == False:  # Can be None
     # We want to disconnect from a passive console.
+    ljinux.based.run("runparts /etc/hooks/disconnect.d/")
     ljinux.based.command.exec("/LjinuxRoot/bin/_waitforconnection.lja")
     term.clear_line()
 else:
