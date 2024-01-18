b00dd3faeae19170c78a1ebd5ad1a64b4e894861
Bill Sideris <bill88t@feline.gr>
2024-01-18 02:13:56 +0200

    Rewrite touch command

--- LjinuxRoot/bin/touch.py
+++ LjinuxRoot/bin/touch.py
@@ -1,10 +1,10 @@
 rename_process("touch")
-try:
-    with ljinux.api.fopen(ljinux.based.user_vars["argj"].split()[1]):
+vr("opts", ljinux.api.xarg())
+
+for pv[get_pid()]["i"] in vr("opts")["w"]:
+    if ljinux.api.isdir(vr("i")) != 2:
         ljinux.based.error(10)
-except OSError:
-    with ljinux.api.fopen(ljinux.based.user_vars["argj"].split()[1], "w") as pv[
-        get_pid()
-    ]["f"]:
-        if vr("f") is None:
-            ljinux.based.error(7)
+    else:
+        with ljinux.api.fopen(vr("i"), "w") as pv[get_pid()]["f"]:
+            if vr("f") is None:
+                ljinux.based.error(7)
