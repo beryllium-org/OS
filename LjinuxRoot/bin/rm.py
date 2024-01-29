6c022044388e179bcb59a480170af67767277dd4
Bill Sideris <bill88t@feline.gr>
2024-01-29 22:42:30 +0200

    Update rm to properly pop code cache if needed.

--- LjinuxRoot/bin/rm.py
+++ LjinuxRoot/bin/rm.py
@@ -8,7 +8,10 @@ try:
 
     for pv[get_pid()]["filee"] in vr("fw"):
         try:
-            remove(ljinux.api.betterpath(vr("filee")))
+            vr("fname", ljinux.api.betterpath(vr("filee")))
+            remove(vr("fname"))
+            if vr("fname") in ljinux.code_cache:
+                ljinux.code_cache.pop(vr("fname"))
         except OSError as errr:
             if str(errr) == "[Errno 2] No such file/directory":
                 ljinux.based.error(4, f=vr("filee"))
