17c46c32996605f0f03588bfbc88e01d0486a9d2
Bill Sideris <bill88t@feline.gr>
2024-02-07 00:57:55 +0200

    Make ftp_serv.py autoconnect to wifi with settings.toml

--- extra/netutils/ftp_serv.py
+++ extra/netutils/ftp_serv.py
@@ -4,11 +4,31 @@ from ftp_server import ftp
 from supervisor import reload
 
 if not wifi.radio.connected:
-    print("No wifi")
+    print("No wifi. Trying to connect via settings.toml")
+
+    from cptoml import keys, fetch
+
+    stored_networks = keys("IWD")
+    available_networks = []
+    for i in wifi.radio.start_scanning_networks():
+        available_networks.append(i.ssid)
+    wifi.radio.stop_scanning_networks()
+    for i in stored_networks:
+        if i in available_networks:
+            try:
+                wifi.radio.connect(i, fetch(i, "IWD"))
+                if wifi.radio.connected:
+                    print("Successfully connected to " + i)
+                    break
+            except:
+                pass
+
+if not wifi.radio.connected:
+    print("Could not connect to wifi!")
     reload()
 
 pool = SocketPool(wifi.radio)
-ftps = ftp(pool, str(wifi.radio.ipv4_address))
+ftps = ftp(pool, str(wifi.radio.ipv4_address), verbose=True)
 print("Starting Ljinux ftp server..")
 try:
     ftps.serve_till_quit()
