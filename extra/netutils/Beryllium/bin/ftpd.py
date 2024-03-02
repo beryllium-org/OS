58fd0d9a0a51d39b3741b5f05393639ea9d992d9
Bill Sideris <bill88t@feline.gr>
2024-03-02 17:17:44 +0200

    ftp as a module

--- extra/netutils/Beryllium/bin/ftpd.py
+++
@@ -1,7 +0,0 @@
-rename_process("ftpd")
-term.write("Exiting to FTP..")
-from supervisor import set_next_code_file
-
-set_next_code_file("ftp_serv.py")
-del set_next_code_file
-be.based.run("reload")
