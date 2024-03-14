b37e3f94a6d5d31de60ecfc5e1672db240e0baf1
Bill Sideris <bill88t@feline.gr>
2024-03-14 06:35:17 +0200

    Add to devices too

--- bootcfg/boot.d/10-builtin-display-init.py
+++ bootcfg/boot.d/10-builtin-display-init.py
@@ -2,8 +2,8 @@ rename_process("builtin display init")
 from drivers.displayiotty import displayiotty
 
 pv[0]["consoles"]["ttyDISPLAY0"] = displayiotty()
-pv[0]["consoles"]["ttyDISPLAY0"].display = be.devices["gpiochip"][0].pin(
-    "DISPLAY", force=True
-)
+be.based.run("mknod DISPLAY")
+be.devices["DISPLAY"][0] = be.devices["gpiochip"][0].pin("DISPLAY", force=True)
+pv[0]["consoles"]["ttyDISPLAY0"].display = be.devices["DISPLAY"][0]
 pv[0]["consoles"]["ttyDISPLAY0"].enable()
 del displayiotty
