b67de8040bf5a8c22e6ba9768e0959872c0594e8
Bill Sideris <bill88t@feline.gr>
2024-05-25 00:35:07 +0300

    Update devid subscript

--- base/Beryllium/bin/stringproccessing/devid.py
+++ base/Beryllium/bin/stringproccessing/devid.py
@@ -1,26 +1,13 @@
 vr("dev_name", "")
-vr("devid_curr", 0)
+vr("devid_curr", len(vr("node")) - 1)
 try:
-    while True:
-        if (vr("node").rfind("_") != -1) and (
-            vr("node")[vr("node").rfind("_") - 1].isdigit()
-        ):
-            if vr("node").rfind("_") == vr("devid_curr"):
-                break
-        else:
-            if vr("node")[vr("devid_curr")].isdigit() or vr("devid_curr") == len(
-                vr("node")
-            ):
-                break
-        vrp("dev_name", vr("node")[vr("devid_curr")])
-        vrp("devid_curr")
-    if vr("node")[vr("devid_curr")] == "_":
-        vrp("devid_curr")
-    vr("dev_id", "")
-    while vr("devid_curr") < len(vr("node")):
-        vrp("dev_id", vr("node")[vr("devid_curr")])
-        vrp("devid_curr")
-    vr("dev_id", int(vr("dev_id")))
+    while vr("devid_curr"):
+        if not vr("node")[vr("devid_curr")].isdigit():
+            break
+        vrm("devid_curr")
+    vrp("devid_curr")
+    vr("dev_name", vr("node")[: vr("devid_curr")])
+    vr("dev_id", int(vr("node")[vr("devid_curr") :]))
     vr("ok", True)
 except IndexError:
     pass
