a795fa05dce07099b16deacaeda23df67c142140
Bill Sideris <bill88t@feline.gr>
2024-05-29 03:28:01 +0300

    Add umount note

--- scripts/wslrmsetter.py
+++ scripts/wslrmsetter.py
@@ -2,5 +2,10 @@ from os import remove
 
 try:
     remove("/tmp/CUSTOMBOARDPATH")
+    print(
+        "Remember to umount from inside of WSL too..\n\n"
+        + "    sudo umount /mnt/D\n"
+        + "    sudo rmdir /mnt/D"
+    )
 except FileNotFoundError:
     pass
