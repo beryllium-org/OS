87abec0583246eb72fc1eadd361fee22f8ebfe79
Bill Sideris <bill88t@feline.gr>
2024-06-01 22:17:31 +0300

    Fix devid to not return stuff ending with _

--- base/Beryllium/bin/stringproccessing/devid.py
+++ base/Beryllium/bin/stringproccessing/devid.py
@@ -7,6 +7,8 @@ try:
         vrm("devid_curr")
     vrp("devid_curr")
     vr("dev_name", vr("node")[: vr("devid_curr")])
+    if vr("dev_name")[-1] == "_":
+        vr("dev_name", vr("dev_name")[:-1])
     vr("dev_id", int(vr("node")[vr("devid_curr") :]))
     vr("ok", True)
 except IndexError:
