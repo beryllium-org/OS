7df55de1aeb25866f66e2ad2ec0f3eea453ce6de
Bill Sideris <bill88t@feline.gr>
2024-08-06 21:47:29 +0300

    Update lspins

--- base/Beryllium/bin/lspins.py
+++ base/Beryllium/bin/lspins.py
@@ -1,6 +1,7 @@
 rename_process("lspins")
 for pv[get_pid()]["i"] in range(len(be.devices["gpiochip"])):
+    term.write("gpiochip" + str(vr("i")) + ":")
     vr("cpins", be.devices["gpiochip"][vr("i")].pins)
     for pv[get_pid()]["j"] in vr("cpins"):
-        term.write(vr("j"))
+        term.write(" " * 4 + vr("j"))
 be.api.setvar("return", "0")
